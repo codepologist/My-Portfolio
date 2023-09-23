bd55570c80f658f232adba52d07233b5d0bf7686
ONeal Ombu <ombu.oneal@yahoo.com>
2023-09-23 18:23:31 +0100

    Added new asset

--- src/assets/index.js
+++ src/assets/index.js
@@ -3,7 +3,7 @@ import project_2 from "./project_2.webp";
 import project_3 from "./project_3.webp";
 import project_4 from "./project_4.webp";
 import project_5 from "./project_5.webp";
-
+import project_6 from "./project_6.webp";
 import project_7 from "./project_7.webp";
 import project_8 from "./project_8.webp";
 
@@ -15,7 +15,7 @@ export {
   project_3,
   project_4,
   project_5,
- 
+  project_6,
   project_7,
   project_8,
   
